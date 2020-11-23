3f80d4e81790fcce2496e450ee1de2d2bd6d2390
fred441a <frederik@altofte.dk>
2020-11-23 15:47:03 +0100

    gjorde rent efter github kursus

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,22 +1,7 @@
 void setup() {
-  // put your setup code here, to run once:
-<<<<<<< HEAD
-  Serial.begin(115200)
+  // put your setup code here, to run once
 }
- ;
-void Jens() {
-  Serial.print("Jeg hedder Jens");
-}
-void loop() {
-  // put your main code here, to run repeatedly:
-=======
-Er linux bedre end windows?
-}
-aasd
-//Noget Gay shit
 
 void loop() {
   // put your main code here, to run repeatedly:
-hejejejejej
->>>>>>> 8b9d10d0e01627691ad00c8da6e043bf577eea93
 }

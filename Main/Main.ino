e7ecee908fce582b46696df7415d621891b8931b
Nikolaj Juel Andersen <nikolaj@juel-andersen.dk>
2020-11-23 14:41:38 +0100

    Jeg har ændre i koden

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,8 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(115200)
 }
 
+void Jens(){
+  Serial.print("Jeg hedder Jens");
+}
 void loop() {
   // put your main code here, to run repeatedly:
 
